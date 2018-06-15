dc97f18e20cb34a3c392b34d76de91fa2dfb91fc
Iga <iga.koninska@gmail.com>
2018-06-15 09:23:03 +0200

    create list of card

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
-
-
+ let card = document.querySelectorAll(".card");
+ let cards = [...card];
+ 
+ let openedCards = []
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -36,3 +38,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
